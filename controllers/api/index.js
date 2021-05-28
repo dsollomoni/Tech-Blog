48d6d16aaca3b3fb274745386c7d5fd12860fc48
Danny Sollomoni <dsollomoni@gmail.com>
2021-05-27 21:40:02 -0400

    bugfixing heroku

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('../../controllers/api/user-routes');
+const postRoutes = require('../../controllers/api/post-routes');
+const commentRoutes = require('../../controllers/api/comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
