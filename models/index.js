7aa36bac49a9003efc86f26f8a8af16a694f3e9d
Danny Sollomoni <dsollomoni@gmail.com>
2021-05-27 22:11:24 -0400

    test

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
-const path = require('path')
-const User = require(path.resolve(__dirname, 'User'));
-const Post = require(path.resolve(__dirname, 'Post'));
-const Comment = require(path.resolve(__dirname, 'Comment'));
+const User = require('./User');
+const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 User.hasMany(Post, {
    foreignKey: 'user_id'
