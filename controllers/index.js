25f219d371b911a01bfa64ccca07a39b92956cca
Danny Sollomoni <dsollomoni@gmail.com>
2021-05-27 21:19:58 -0400

    bugs

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('../controllers/home-routes');
+const dashboardRoutes = require('../controllers/dashboard-routes');
 
 const apiRoutes = require('./api');
 
