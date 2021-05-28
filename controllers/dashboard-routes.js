c961263e63cbd047a4a715a87f2120260eb9028d
Danny Sollomoni <dsollomoni@gmail.com>
2021-05-27 23:48:47 -0400

    Fixed the edit route:

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -48,7 +48,8 @@ router.get('/', withAuth, (req, res) => {
 router.get('/edit/:id', withAuth, (req, res) => {
    Post.findOne({
       where: {
-         user_id: req.session.user_id
+         user_id: req.session.user_id,
+         id: req.params.id
       },
       attributes: [
          'id',
